024f9ae1781363ca9fdf007542bba2e2e0668b7c
yy933 <emilyyu025@gmail.com>
2025-06-05 13:20:04 +0800

    feat: update the logic in getRandomSample function

--- src/utils/getRandomSample.ts
+++ src/utils/getRandomSample.ts
@@ -1,6 +1,13 @@
-export function getRandomSample<T>(array: T[], size: number): T[] {
-  if (array.length <= size) return array;
-  const maxStartIndex = array.length - size;
-  const startIndex = Math.floor(Math.random() * (maxStartIndex + 1));
-  return array.slice(startIndex, startIndex + size);
+export function getRandomSample<T>(data: T[], size: number): T[] {
+  if (data.length <= size) return data;
+  const randomIndicesArray: number[] = [];
+  for (let i = 0; i < size; i++) {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    if (!randomIndicesArray.includes(randomIndex)) {
+      randomIndicesArray.push(randomIndex);
+    } else {
+      i--;
+    }
+  }
+  return randomIndicesArray.map((index) => data[index]);
 }
