29eb046814be2b57d013dc80c8fc2623c6541abe
yy933 <emilyyu025@gmail.com>
2025-06-10 17:25:29 +0800

    feat: rename state var isGameOver into areAllCardsMatched

--- src/stores/useEmojiStore.ts
+++ src/stores/useEmojiStore.ts
@@ -6,20 +6,20 @@ type EmojiStore = {
   emojisdata: EmojiData[];
   isGameOn: boolean;
   matchedCards: { name: string; index: number }[];
-  isGameOver: boolean;
+  areAllCardsMatched: boolean;
   setEmojis: (emojis: EmojiData[]) => void;
   setGameOn: (isGameOn: boolean) => void;
   setMatchedCards: (cards: { name: string; index: number }[]) => void;
-  setGameOver: (isGameOver: boolean) => void;
+  setareAllCardsMatched: (isGameOver: boolean) => void;
 };
 
 export const useEmojiStore = create<EmojiStore>((set) => ({
   emojisdata: [],
   isGameOn: false,
   matchedCards: [],
-  isGameOver: false,
+  areAllCardsMatched: false,
   setEmojis: (emojis) => set({ emojisdata: emojis }),
   setGameOn: (isGameOn) => set({ isGameOn }),
   setMatchedCards: (cards) => set({ matchedCards: cards }),
-  setGameOver: (isGameOver) => set({ isGameOver }),
+  setareAllCardsMatched: (areAllCardsMatched) => set({ areAllCardsMatched }),
 }));
