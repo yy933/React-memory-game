62c9c88f7a9204da105ffbb19b23210c33b43189
yy933 <emilyyu025@gmail.com>
2025-06-11 13:59:14 +0800

    feat: add gameover and confetti when all cards are matched.

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -5,11 +5,15 @@ import { useGameLogic } from "@/hooks/useGameLogic";
 import { useMatchingLogic } from "@/hooks/useMatchingLogic";
 import MemoryCardList from "@/components/MemoryCardList";
 import AssistiveTechInfo from "@/components/AssistiveTechInfo";
+import GameOver from "@/components/GameOver";
+import Confetti from "react-confetti";
 
 export default function GamePage() {
   const emojisData = useEmojiStore((state) => state.emojisdata);
+  const areAllCardsMatched = useEmojiStore((state) => state.areAllCardsMatched);
   const { getEmojiDatafromAPI } = useGameLogic();
   const { turnCard, selectedCards, matchedCards } = useMatchingLogic();
+
   useEffect(() => {
     if (emojisData.length === 0) {
       getEmojiDatafromAPI();
@@ -19,6 +23,12 @@ export default function GamePage() {
   return (
     <main>
       <AssistiveTechInfo emojisData={emojisData} matchedCards={matchedCards} />
+      {typeof window !== "undefined" && areAllCardsMatched && (
+        <>
+          <GameOver />
+          <Confetti width={window.innerWidth} height={window.innerHeight} />
+        </>
+      )}
       <MemoryCardList
         handleClickAction={turnCard}
         data={emojisData}
---
+++ src/components/GameOver.tsx
@@ -0,0 +1,7 @@
+export default function GameOver(){
+  return (
+    <div className="wrapper wrapper--accent">
+      <p className="p--large">You&apos;ve matched all the memory cards!</p>
+    </div>
+  );
+}
