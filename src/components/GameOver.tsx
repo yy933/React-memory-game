1730f44257220d66d7d3522acbcc6ec3917d35a2
yy933 <emilyyu025@gmail.com>
2025-06-11 18:46:03 +0800

    feat: make GameOver component accessible

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -2,10 +2,15 @@ import RegularButton from "./RegularButton";
 import { GameOverProps } from "@/types";
 import HomeLink from "./HomeLink";
 import Link from "next/link";
+import { useRef, useEffect } from "react";
 
 export default function GameOver({ handleClick }: GameOverProps) {
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    ref.current?.focus();
+  }, []);
   return (
-    <div className="wrapper wrapper--accent">
+    <div className="wrapper wrapper--accent" ref={ref} tabIndex={-1}>
       <p className="p--large">You&apos;ve matched all the memory cards!</p>
       <RegularButton handleClick={handleClick}>
         <Link href="/game">Play again</Link>
